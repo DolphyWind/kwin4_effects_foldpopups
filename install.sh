3721388a544d8e2a0813dc1d9cffbee025c2c621
HeCheng Yu <kde-yyds@qq.com>
2025-01-04 10:57:57 +0800

    Fix incorrect path in install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Set effect name
+export NAME="foldingpopups"
+
 # Check if DESTDIR is set
 if [ -z "$DESTDIR" ]; then
     # If DESTDIR is not set and the user is root, set it to /usr
     if [ "$(id -u)" -eq 0 ]; then
-        DESTDIR="/usr"
+        DESTDIR="/usr/share"
     else
         # Otherwise, set it to the user's home directory
-        DESTDIR="$HOME"
+        DESTDIR="$HOME/.local/share"
     fi
 fi
 
@@ -15,6 +18,6 @@ fi
 mkdir -p "$DESTDIR/kwin/effects"
 
 # Copy the 'foldingpopups' directory to $DESTDIR/kwin/effects/
-cp -r foldingpopups "$DESTDIR/kwin/effects/"
+cp -r "$NAME" "$DESTDIR/kwin/effects/"
 
-echo "foldingpopups has been copied to $DESTDIR/kwin/effects/"
+echo "$NAME has been copied to $DESTDIR/kwin/effects/"
